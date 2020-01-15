28145631e5a67ab6053835a7b84eca2d2f1c1a8e
Alexander K <alexander_kochurin@epam.com>
2020-01-15 14:36:21 +0300

    Added record. Hard refactoring - drfat

--- ak_tools/src/main/java/g/tools/statistic/commands/CLIHandler.java
+++ ak_tools/src/main/java/g/tools/statistic/commands/CLIHandler.java
@@ -1,10 +1,10 @@
 package g.tools.statistic.commands;
 
+
 import g.tools.statistic.models.Statistics;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+
 
 public class CLIHandler {
 
@@ -21,21 +21,32 @@ public class CLIHandler {
     }
 
     private String currentFilePath;
-    Statistics statistics = new Statistics();
-    private StandardCommands standardCommands = new StandardCommands(statistics);
+    private final Statistics statistics;
+    private final Scanner scanner;
+    private final StandardCommands standardCommands;
+    private final ICommandWrapper commandsChain;
+
 
     public CLIHandler() {
+        this.statistics = new Statistics();
+        this.standardCommands = new StandardCommands(this.statistics);
+        this.scanner = new Scanner(System.in);
+
+        this.commandsChain = new FilePathInput();
+        this.commandsChain
+                .setNextCommand(new SaveNextInput())
+                .setNextCommand(new AddRecordInput())
+                .setNextCommand(new AddsRecordInput());
     }
 
     public void run() {
         System.out.println("Start the program...");
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a command:");
 
-        while (nextStep(scanner, null, null) != StepCode.EXIT);
+        while (nextStep(null, null) != StepCode.EXIT);
     }
 
-    private StepCode nextStep(Scanner scanner, Command inputCommand, Object sInput) {
+    private StepCode nextStep(Command inputCommand, Object sInput) {
         Command command = inputCommand == null ? Command.parse(scanner.nextLine()) : inputCommand;
         if (command == Command.EXIT) {
             return StepCode.EXIT;
@@ -47,48 +58,122 @@ public class CLIHandler {
 
         // TODO use chain of responsibilities pattern.
         if (input == null) {
-            switch (command) {
-                case SAVE_FILE:
-                case READ_FILE:
-                case LOAD_FILE:
-                    String filePath = getFilePathFromCLI(scanner);
-                    this.currentFilePath = filePath;
-                    input = filePath;
-                    break;
-                case SAVE_NEXT_FILE:
-                    input = this.currentFilePath;
-                    break;
-                case ADD_RECORD:
-                    input = scanner;
-                    break;
-                case ADD_S:
-                    Map<String, Object> parameters = new HashMap<>();
-                    parameters.put("scanner", scanner);
-                    parameters.put("filePath", this.currentFilePath);
-                    input = parameters;
-                    break;
-                default:
-                    //throw new RuntimeException("Unknown command: " + command);
-                    System.out.println("Unknown command: " + command);
-                    return StepCode.UNKNOWN_COMMAND;
-            }
+            input = this.commandsChain.handle(command);
+            //throw new RuntimeException("Unknown command: " + command);
+//            System.out.println("Unknown command: " + command);
+//            return StepCode.UNKNOWN_COMMAND;
         }
 
-        runCommand(command, input);
+//        System.out.println(command + " ----> " + input);
+        CommandExecutor commandExecutor = standardCommands.getCommands().get(command);
+        commandExecutor.execute(input);
         System.out.println("Command " + command + " executed");
 
         return StepCode.OK;
     }
 
-    private static String getFilePathFromCLI(Scanner scanner) {
-        System.out.println("Enter file path: ");
-        String filePath = scanner.nextLine();
-        return filePath;
+    // TODO Do something with it.
+
+    private interface ICommandWrapper {
+        Object handle(Command command);
+        boolean canHandle(Command command);
+        ICommandWrapper getNextCommand();
+        ICommandWrapper setNextCommand(ICommandWrapper command);
     }
 
-    private Object runCommand(Command command, Object input) {
-        CommandExecutor commandExecutor = standardCommands.getCommands().get(command);
-        return commandExecutor.execute(input);
+    private abstract class CommandWrapper implements ICommandWrapper {
+
+        private ICommandWrapper commandInputWrapper;
+
+        @Override
+        public ICommandWrapper getNextCommand() {
+            return this.commandInputWrapper;
+        }
+
+        @Override
+        public ICommandWrapper setNextCommand(ICommandWrapper command) {
+            this.commandInputWrapper = command;
+            return this.commandInputWrapper;
+        }
+
+        @Override
+        public Object handle(Command command) {
+            Object res;
+            if (canHandle(command)) {
+                res = internalHandle(command);
+            } else {
+                ICommandWrapper nextChain = getNextCommand();
+                res = nextChain == null ? null : nextChain.handle(command);
+            }
+            return res;
+        }
+
+        protected abstract Object internalHandle(Command command);
+    }
+
+
+    private class FilePathInput extends CommandWrapper {
+
+        @Override
+        public boolean canHandle(Command command) {
+            return command == Command.SAVE_FILE
+                    || command == Command.LOAD_FILE
+                    || command == Command.READ_FILE;
+        }
+
+        @Override
+        public Object internalHandle(Command command) {
+            System.out.println("Enter file path: ");
+            String filePath = scanner.nextLine();
+            CLIHandler.this.currentFilePath = filePath;
+            return filePath;
+        }
+
+    }
+
+    private class SaveNextInput extends CommandWrapper {
+
+        @Override
+        public boolean canHandle(Command command) {
+            return command == Command.SAVE_NEXT_FILE;
+        }
+
+        @Override
+        public Object internalHandle(Command command) {
+            return CLIHandler.this.currentFilePath;
+        }
+
+    }
+
+    private class AddRecordInput extends CommandWrapper {
+
+        @Override
+        public boolean canHandle(Command command) {
+            return command == Command.ADD_RECORD;
+        }
+
+        @Override
+        public Object internalHandle(Command command) {
+            return CLIHandler.this.scanner;
+        }
+
+    }
+
+    private class AddsRecordInput extends CommandWrapper {
+
+        @Override
+        public boolean canHandle(Command command) {
+            return command == Command.ADD_S;
+        }
+
+        @Override
+        public Object internalHandle(Command command) {
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("scanner", scanner);
+            parameters.put("filePath", CLIHandler.this.currentFilePath);
+            return parameters;
+        }
+
     }
 
 }
